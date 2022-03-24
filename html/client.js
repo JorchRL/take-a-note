ca4f7671d36d8482cd9f91dd62f362c477aaaac7
Jorge Romero Lagunas <jorchrl@Jorges-MacBook-Pro.local>
2022-03-23 22:34:14 -0600

    Add client html css and js files

---
+++ html/client.js
@@ -0,0 +1,14 @@
+
+// We want to play with this elements
+const articleList = document.getElementById("#article-list")
+const article = document.getElementById("#article-display")
+const deleteButton = document.getElementById("#delete-note")
+const copyButton = document.getElementById("#copy-markdown")
+
+// 
+
+
+
+/// Functions
+
+const fetchFromDB = () => {}
